72a3e7061d9af6f4161a857dcc114251661f5fe7
Bommer <quangthi_90@yahoo.com.vn>
2015-11-02 08:43:54 +0700

    - Change home.html to home.php
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

--- home.php
+++ home.php
@@ -3,10 +3,11 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+<?php include 'language.php'; ?>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Empyrean Design - Home</title>
+        <title>Empyrean Design - <?php echo $_SESSION['home']; ?></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
