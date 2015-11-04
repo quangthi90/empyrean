2720a045e695e398f55dc7a9b277b44c0b9bbb0c
Bommer <quangthi_90@yahoo.com.vn>
2015-11-05 00:15:36 +0700

    - Update languages for aboutus
    Signed-off-by: Bommer <quangthi_90@yahoo.com.vn>

--- aboutus.php
+++ aboutus.php
@@ -11,33 +11,13 @@
       <div class="content-block">
          <div class="overlay"></div>                   
          <div class="content-block-details">
-             <h3 class="title right">Our Mission</h3>                       
+             <h3 class="title right"><?php echo $_SESSION['our_mission']; ?></h3>                       
              <div class="clear"></div>
              <div class="img-wrapper">
                  <img src="img/about/vision.png" alt="Our Mission">
              </div>                       
              <div class="content">
-                 <p>
-                 It is our belief that nothing exists by itself in this world. We co-exist with one another and we are interdependent on one another. This creates greater dimension and contributes colours to the earth. Good designs are not good enough; a great product is one that will be able to sell itself. We believe that the design that sells is the design that is most relevant and appealing to the consumer, besides the aesthetic aspect. We are ahead of design trends, and in fact, trend setters because we dare to be different in our thinking process. We are critical in design concepts, yet able to provide long terms solutions. We possess the know how in integrating components and issues that will provide comprehensive, practical and economic solutions in terms of operations of human resources, operating cost, and profit. One of our main concern is in providing an outstanding design but also in the preservation of our clients resources as well.
-                 </p>
-                 <p class="center">
-                  <strong> - We dare to face the negative issues, for we will have the positive solutions to overcome them. - <br> Eric's Haywood Chang's quote 
-                  </strong>
-                 </p>
-                 <p>
-                     Our company believes in giving back to the society and to the world. We are actively helping students around the world in sharing designs and critics. In addition, we have setup a design forum to facilitate this function. We wish to help our future young design leaders to dream, explore, and stay passionate in what they believe in and love doing. <br>
-                     In supporting the music industries, we provide special competitive costing for artistes and related field clients in the acoustic environment build up. <br>
-                      We are also involved in the feeding the third world program.
-                 </p>
-                 <p class="center">
-                 This ties in with our vision: <strong>Shaping our future today</strong>
-                 </p>
-                 <p>
-                     We are shaping our future by investing and impacting in others now. <br>
-                      Dare to dream, turning dreams into reality. <br>
-                      From drawings to buildings, building a better tomorrow.<br>
-                      We are who we are now because our clients believes in our dreams.
-                 </p>
+                 <?php echo $_SESSION['mission_content']; ?>
              </div>
          </div>
      </div>
@@ -45,27 +25,13 @@
      <div class="content-block">
          <div class="overlay"></div>                   
          <div class="content-block-details">
-             <h3 class="title right">Our Story</h3>                       
+             <h3 class="title right"><?php echo $_SESSION['our_story']; ?></h3>                       
              <div class="clear"></div>
              <div class="img-wrapper">
                  <img src="img/about/company.png" alt="Our story">
              </div>                       
              <div class="content">
-                 <p>
-                 Empyrean Design was founded in 1992 by Eric Haywood Chang. Having graduated from LaSalle College of the Arts in Singapore; majoring in Interior Designing and minor in Product Designing and Fine Arts, he is a self-taught architect that possesses many years of experience and expertise in the related field. He is passionate about design, arts, and music.
-                 </p>
-                 <p>
-                  From day one, our main focus has been in interior designing. We have completed many projects, ranging from residential to corporate clients. Our core strength has always been in the area of identifying our clients' image and their needs during our design process. We are capable of developing new and current corporate images for new company startups as well as for companies that are looking into changing their corporate image. Our clients range from retail, corporate companies, hospitality, to government agencies. We walk through the entire design process with our clients from the ground zero to the completion of each projects. We work closely with our dedicated team of architectures, designers, professional engineers, and contractors under our wing. We are able to customize our scope of work as per our client's requirement from design stage, project management, construction stage and to handing over of project to our client. 
-                 </p>
-                 <p>
-                    Since 2004, due to our strong connections in China, we have branched into architecture from interior designing. We have had fruitful cooperation with many of our China counterparts in different cities and provinces of China. However, our main focal point has been in the Hainan Province due to it having the highest numbers of hotels and resorts; the "Hawaii of China" as many have named. Recently, we have surveyed sites with drones for a more thorough inspection and recommend the sites which are optimal for our clients usage.                       
-                 </p>
-                 <p>
-                 In 2008, we ventured into the specialization of acoustic environments whether it be for supporting artistes or sound engineers in the region. Thereafter, we involved ourselves in the designing and building up of different environments, from casino gaming spaces, indoor firing ranges, lecture halls, recording studios, broadcasting rooms, home theatres, etc. 
-                 </p>
-                 <p>
-                 In 2014, we entered Myanmar market. We are slowly gaining momentum in the design & construction industries.
-                 </p>
+                <?php echo $_SESSION['story_content']; ?>
              </div>
          </div>
      </div>
